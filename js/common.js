bd9d388b0a894bd4634c2592a82c5d08540bfbc3
Andrew <avenuce@rambler.ru>
2017-03-29 00:16:54 +0300

    cleare not-needed parts of code

--- js/common.js
+++ js/common.js
@@ -43,12 +43,12 @@ $(window).resize(function(){
 
 //ф-ия, которая делает тыбадля телефона
 $(".top_phone .wrapper .tab").click(function() {
-    $(".top_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
-    $(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()
+	$(".top_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
 $(".tabs_header .wrapper .tab").click(function() {
-    $(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
-    $(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()
+	$(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
 
 
