aaa4fe91ecd83098e452c98b16c99d9e845e2954
Andrew <avenuce@rambler.ru>
2017-04-05 23:21:22 +0300

    footer donne

--- js/common.js
+++ js/common.js
@@ -50,6 +50,11 @@ $(".tabs_header .wrapper .tab").click(function() {
 	$(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
 	$(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
+$(".bot_phone .wrapper .tab").click(function() {
+	$(".bot_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".bot_phone .tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+
 
 
 
